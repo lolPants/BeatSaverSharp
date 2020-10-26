6798de3f7659b63c1fe41d8bc35266f729d6775c
Jack Baron <jackmbaron@gmail.com>
2020-10-26 22:06:57 +0000

    clean house

--- BeatSaverSharp/Exceptions/InvalidTicketException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BeatSaverSharp.Exceptions
-{
-    /// <summary>
-    /// Thrown when an invalid auth ticket is used to vote with
-    /// </summary>
-    public class InvalidTicketException : Exception { }
-}
