61ead009b3506232ba7bb087dbc4639535030c48
Jack Baron <jackmbaron@gmail.com>
2020-10-27 05:24:50 +0000

    impelement paginated methods

--- BeatSaverSharp/BeatSaver.cs
+++ BeatSaverSharp/BeatSaver.cs
@@ -50,6 +50,27 @@ namespace BeatSaverSharp
         {
             return await FetchSingle($"/stats/hash/{hash}", options).ConfigureAwait(false);
         }
+
+        internal async Task<Page?> FetchPaged(string url, PagedRequestOptions options)
+        {
+            var request = WebRequest.FromOptions(url, options);
+            var resp = await HttpInstance.GetAsync(request).ConfigureAwait(false);
+            if (resp.StatusCode == HttpStatusCode.NotFound) return null;
+
+            Page? p = resp.JSON<Page>();
+            if (p is null) return null;
+
+            p.Client = this;
+            p.Options = options;
+
+            foreach (var b in p.Docs)
+            {
+                b.Client = this;
+                b.Uploader.Client = this;
+            }
+
+            return p;
+        }
         #endregion
 
         #region Single Beatmap Methods
@@ -75,6 +96,52 @@ namespace BeatSaverSharp
         {
             if (hash is null) throw new ArgumentNullException(nameof(hash));
             return await FetchSingle($"/maps/by-hash/{hash}", options ?? BeatmapRequestOptions.Default).ConfigureAwait(false);
+        }
+        #endregion
+
+        #region Paginated Methods
+        /// <summary>
+        /// Fetch a page of beatmaps, ordered by upload date
+        /// </summary>
+        /// <param name="options">Request Options</param>
+        /// <returns></returns>
+        public async Task<Page> Latest(PagedRequestOptions? options = null)
+        {
+            var page = await FetchPaged($"/maps/latest", options ?? PagedRequestOptions.Default).ConfigureAwait(false);
+            return page!;
+        }
+
+        /// <summary>
+        /// Fetch a page of beatmaps, ordered by heat score
+        /// </summary>
+        /// <param name="options">Request Options</param>
+        /// <returns></returns>
+        public async Task<Page> Hot(PagedRequestOptions? options = null)
+        {
+            var page = await FetchPaged($"/maps/hot", options ?? PagedRequestOptions.Default).ConfigureAwait(false);
+            return page!;
+        }
+
+        /// <summary>
+        /// Fetch a page of beatmaps, ordered by rating
+        /// </summary>
+        /// <param name="options">Request Options</param>
+        /// <returns></returns>
+        public async Task<Page> Rating(PagedRequestOptions? options = null)
+        {
+            var page = await FetchPaged($"/maps/rating", options ?? PagedRequestOptions.Default).ConfigureAwait(false);
+            return page!;
+        }
+
+        /// <summary>
+        /// Fetch a page of beatmaps, ordered by download count
+        /// </summary>
+        /// <param name="options">Request Options</param>
+        /// <returns></returns>
+        public async Task<Page> Downloads(PagedRequestOptions? options = null)
+        {
+            var page = await FetchPaged($"/maps/downloads", options ?? PagedRequestOptions.Default).ConfigureAwait(false);
+            return page!;
         }
         #endregion
     }
