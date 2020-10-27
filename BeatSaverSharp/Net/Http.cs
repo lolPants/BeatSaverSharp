5c1da5fc6d599677f3f4507a005ea10d397a61e8
Jack Baron <jackmbaron@gmail.com>
2020-10-27 09:20:16 +0000

    remove and sort usings

--- BeatSaverSharp/Net/Http.cs
+++ BeatSaverSharp/Net/Http.cs
@@ -1,12 +1,12 @@
 using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 using BeatSaverSharp.Exceptions;
-using System.IO;
+using Newtonsoft.Json;
 
 namespace BeatSaverSharp
 {
--- BeatSaverSharp/Types/Beatmap/Beatmap.cs
+++ BeatSaverSharp/Types/Beatmap/Beatmap.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using BeatSaverSharp.Exceptions;
+using Newtonsoft.Json;
 
 namespace BeatSaverSharp
 {
