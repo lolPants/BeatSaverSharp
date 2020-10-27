cd15d3d2c400e044f31fba7e7332ba7b14412e41
Jack Baron <jackmbaron@gmail.com>
2020-10-27 04:20:06 +0000

    re-implement single map request methods

--- BeatSaverSharp/BeatSaver.cs
+++ BeatSaverSharp/BeatSaver.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BeatSaverSharp
 {
@@ -26,6 +28,50 @@ namespace BeatSaverSharp
         #region Properties
         internal Http HttpInstance { get; }
         internal HttpClient HttpClient { get => HttpInstance.Client; }
+        #endregion
+
+        #region Internal Methods
+        internal async Task<Beatmap?> FetchSingle(string url, BeatmapRequestOptions options)
+        {
+            var request = WebRequest.FromOptions(url, options);
+            var resp = await HttpInstance.GetAsync(request).ConfigureAwait(false);
+            if (resp.StatusCode == HttpStatusCode.NotFound) return null;
+
+            Beatmap? b = resp.JSON<Beatmap>();
+            if (b is null) return null;
+
+            b.Client = this;
+            // TODO: Add client to beatmap uploader class
+            // b.Uploader.Client = this;
+
+            return b;
+        }
+        #endregion
+
+        #region Single Beatmap Methods
+        /// <summary>
+        /// Fetch a Beatmap by Key
+        /// </summary>
+        /// <param name="key">Hex Key</param>
+        /// <param name="options">Request Options</param>
+        /// <returns></returns>
+        public async Task<Beatmap?> Key(string? key, BeatmapRequestOptions? options = null)
+        {
+            if (key is null) throw new ArgumentNullException(nameof(key));
+            return await FetchSingle($"/stats/key/{key}", options ?? BeatmapRequestOptions.Default).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Fetch a Beatmap by Hash
+        /// </summary>
+        /// <param name="hash">SHA1 Hash</param>
+        /// <param name="options">Request Options</param>
+        /// <returns></returns>
+        public async Task<Beatmap?> Hash(string? hash, BeatmapRequestOptions? options = null)
+        {
+            if (hash is null) throw new ArgumentNullException(nameof(hash));
+            return await FetchSingle($"/maps/by-hash/{hash}", options ?? BeatmapRequestOptions.Default).ConfigureAwait(false);
+        }
         #endregion
     }
 }
