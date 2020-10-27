560ecac8b022550b360c7f857eeffdceaa7129b1
Jack Baron <jackmbaron@gmail.com>
2020-10-27 05:08:00 +0000

    override user equality

--- BeatSaverSharp/Types/User.cs
+++ BeatSaverSharp/Types/User.cs
@@ -25,5 +25,30 @@ namespace BeatSaverSharp
         [JsonIgnore]
         internal BeatSaver? Client { get; set; }
         #endregion
+
+        #region Equality
+        /// <summary>
+        /// Check for value equality
+        /// </summary>
+        /// <param name="u">User to compare against</param>
+        /// <returns></returns>
+        public bool Equals(User u)
+        {
+            if (u is null) return false;
+            if (ReferenceEquals(this, u)) return true;
+            if (GetType() != u.GetType()) return false;
+
+            return (ID == u.ID);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            if (ID is not null) return ID.GetHashCode();
+            throw new NullReferenceException("ID should not be null!");
+        }
+        #endregion
     }
 }
