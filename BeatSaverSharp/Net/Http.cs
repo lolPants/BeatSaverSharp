93f3d74fb5e9c7ab183e93e842765bdb9cc2fd14
Jack Baron <jackmbaron@gmail.com>
2021-01-06 03:02:22 +0000

    remove unused using

--- BeatSaverSharp/Net/Http.cs
+++ BeatSaverSharp/Net/Http.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using BeatSaverSharp.Exceptions;
